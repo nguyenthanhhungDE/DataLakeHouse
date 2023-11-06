fc8a8cd9275abe7b5c0eca8e4060bd7a93fca2d6
ferr2 <thaiduy756@gmail.com>
2023-11-06 09:38:36 +0700

    update file load_data.sql

--- load_dataset_into_mysql/load_data.sql
+++ load_dataset_into_mysql/load_data.sql
@@ -1,17 +1,17 @@
 LOAD DATA local INFILE '/tmp/dataset/product_category_name_translation.csv'
-INTO TABLE product_category_name_translation FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\n'
+INTO TABLE product_category_name_translation FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
 LOAD DATA local INFILE '/tmp/dataset/olist_sellers_dataset.csv'
-INTO TABLE sellers FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\n'
+INTO TABLE sellers FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
 LOAD DATA local INFILE '/tmp/dataset/olist_customers_dataset.csv'
-INTO TABLE customers FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\n'
+INTO TABLE customers FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
 LOAD DATA local INFILE '/tmp/dataset/olist_products_dataset.csv'
-INTO TABLE products FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\n'
+INTO TABLE products FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 	
 LOAD DATA local INFILE '/tmp/dataset/olist_orders_dataset.csv'
@@ -19,11 +19,11 @@ INTO TABLE orders FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '
 IGNORE 1 ROWS;
 
 LOAD DATA local INFILE '/tmp/dataset/olist_order_items_dataset.csv'
-INTO TABLE order_items FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\n'
+INTO TABLE order_items FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
 LOAD DATA local INFILE '/tmp/dataset/olist_order_payments_dataset.csv'
-INTO TABLE payments FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\n'
+INTO TABLE payments FIELDS TERMINATED BY ',' enclosed by '"' LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
 LOAD DATA local INFILE '/tmp/dataset/olist_order_reviews_dataset.csv'
