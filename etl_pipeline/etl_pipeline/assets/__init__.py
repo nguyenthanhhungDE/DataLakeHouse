9f46c9b7eaa862bcee41a036f88b1797e6342633
hungnguyenthanh <nguyenthhung2k2@gmail.com>
2023-11-22 22:28:24 +0700

    update

--- etl_pipeline/etl_pipeline/assets/__init__.py
+++ etl_pipeline/etl_pipeline/assets/__init__.py
@@ -1,7 +1,8 @@
 from dagster import load_assets_from_modules, file_relative_path
 
-from . import bronze,silver
-
+from . import bronze,silver,gold,platium
 
 bronze_layer_assets = load_assets_from_modules([bronze])
 silver_layer_assets = load_assets_from_modules([silver])
+gold_layer_assets = load_assets_from_modules([gold])
+platium_layer_assets = load_assets_from_modules([platium])
